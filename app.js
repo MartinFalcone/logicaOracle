83d12b32c84c35c75cecc125494fbe9635577d67
MartinFalcone <tinchofalcone@gmail.com>
2024-03-06 18:50:09 -0300

    upedated 1

--- app.js
+++ app.js
@@ -1,26 +1,10 @@
-// let numeroSecreto = 6
-// let numeroUsuario = prompt("Dime un numero para adivinar");
+let numeroSecreto = 6
+let numeroUsuario = prompt("Dime un numero para adivinar");
 
-// console.log(numeroUsuario);
+console.log(numeroUsuario);
 
-// if (numeroUsuario == numeroSecreto){
-//     alert("acertaste el numero");
-// } else {
-//     alert(`no acertaste el numero ${numeroUsuario}`)
-// }
-
-
-
-let porcentajeDescuento = 0;
-
-let cantidadMillas = prompt("Ingrese la cantidad de millas");
-
-if (cantidadMillas > 30000) {
-    porcentajeDescuento = 20;	
-} else if (cantidadMillas > 5000) {
-    porcentajeDescuento = 10;	
+if (numeroUsuario == numeroSecreto){
+    alert(`acertaste el numero ${numeroSecreto}` );
 } else {
-    porcentajeDescuento = 0;
+    alert("no acertaste el numero");
 }
-
-alert(porcentajeDescuento)
